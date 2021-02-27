27c16a655838fc7e958284872f1db1881144b9f3
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-02-27 13:31:21 -0600

    BlogPost model updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -13,6 +14,13 @@ db = SQLAlchemy(app)
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DataTime, nullable=False,
+                            default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post' + str(self.id)
 
 
 all_posts = [
