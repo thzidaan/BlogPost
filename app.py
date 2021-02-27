f368606d9fdd0cf943190d2dc2181f806b38d208
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-02-27 14:41:52 -0600

    Add functionality added

--- app.py
+++ app.py
@@ -34,8 +34,9 @@ def posts():
     if request.method == 'POST':
         post_title = request.form['title']
         post_content = request.form['content']
+        post_author = request.form['author']
         new_post = BlogPost(
-            title=post_title, content=post_content, author='Zidaan')
+            title=post_title, content=post_content, author=post_author)
 
         db.session.add(new_post)
         db.session.commit()
@@ -44,6 +45,16 @@ def posts():
     else:
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
+
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect('/posts')
 
 
 if __name__ == "__main__":
