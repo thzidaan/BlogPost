28b6914ac8536017b354f3241300b941461225e4
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-02-27 14:22:46 -0600

    Front end connected to Backend for Post reqs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,26 +16,11 @@ class BlogPost(db.Model):
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(20), nullable=False, default='N/A')
-    date_posted = db.Column(db.DataTime, nullable=False,
+    date_posted = db.Column(db.DateTime, nullable=False,
                             default=datetime.utcnow)
 
     def __repr__(self):
         return 'Blog post' + str(self.id)
-
-
-all_posts = [
-    {
-        'title': 'Post 1',
-        'content': 'This is the content of post 1',
-        'author': 'Eren'
-
-    },
-    {
-        'title': 'Post 2',
-        'content': 'This is the content of post 2'
-
-    }
-]
 
 
 @app.route('/home')
@@ -43,9 +28,22 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template('posts.html', posts=all_posts)
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        new_post = BlogPost(
+            title=post_title, content=post_content, author='Zidaan')
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts=all_posts)
 
 
 if __name__ == "__main__":
